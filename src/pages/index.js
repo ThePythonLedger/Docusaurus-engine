099ed98031ab2b7b0b66aeaddf5b2cea63e9fc75
razorblade23 <josip.vivo.cubric@gmail.com>
2026-03-18 23:26:04 +0100

    Fix the route

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/category/course-overview">
+            to="/foundations">
             Start Your First Lesson - 5min ⏱️
           </Link>
         </div>
