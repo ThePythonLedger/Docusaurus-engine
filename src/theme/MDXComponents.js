a8d838dc3ca7f823dff8682ec99b0040f9baedd8
razorblade23 <josip.vivo.cubric@gmail.com>
2026-09-15 13:37:01 +0200

    Added MemoryGraph component and new workflow to build preview pages

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,14 +1,18 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import InteractivePython from '@site/src/components/InteractivePython';
+import MemoryGraphBlock from '@site/src/components/MemoryGraphBlock';
 
 export default {
   ...MDXComponents,
   // We wrap the standard 'pre' tag (which wraps code blocks)
   pre: (props) => {
-    const isInteractive = props.children?.props?.metastring?.includes('interactive');
-    
-    if (isInteractive) {
+    const metastring = props.children?.props?.metastring || '';
+
+    if (metastring.includes('interactive')) {
       return <InteractivePython {...props} />;
+    }
+    if (metastring.includes('debug')) {
+      return <MemoryGraphBlock {...props} />;
     }
     return <MDXComponents.pre {...props} />;
   },
